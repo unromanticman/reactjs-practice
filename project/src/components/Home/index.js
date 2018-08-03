90a07b050a82447176bb906b767c9ca745cbff29
unknown <tis.lin.77318@linkchain.tw.local>
2018-08-03 16:57:39 +0800

    刪減realworld

---
+++ project/src/components/Home/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import agent from '../../agent';
+import { connect } from 'react-redux';
+import {
+  HOME_PAGE_LOADED,
+  HOME_PAGE_UNLOADED,
+  APPLY_TAG_FILTER
+} from '../../constants/actionTypes';
+
+const Promise = global.Promise;
+
+const mapStateToProps = state => ({
+  ...state.home,
+  appName: state.common.appName,
+  token: state.common.token
+});
+
+const mapDispatchToProps = dispatch => ({
+  onClickTag: (tag, pager, payload) =>
+    dispatch({ type: APPLY_TAG_FILTER, tag, pager, payload }),
+  onLoad: (tab, pager, payload) =>
+    dispatch({ type: HOME_PAGE_LOADED, tab, pager, payload }),
+  onUnload: () =>
+    dispatch({  type: HOME_PAGE_UNLOADED })
+});
+
+class Home extends React.Component {
+  componentWillMount() {
+    const tab = this.props.token ? 'feed' : 'all';
+    const articlesPromise = this.props.token ?
+      agent.Articles.feed :
+      agent.Articles.all;
+    this.props.onLoad(tab, articlesPromise, Promise.all([agent.Tags.getAll(), articlesPromise()]));
+  }
+
+  componentWillUnmount() {
+    this.props.onUnload();
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+
+         <div className="banner">
+            <div className="container">
+              <h1 className="logo-font">
+                test
+              </h1>
+              <p>A place to share your knowledge.</p>
+            </div>
+          </div>
+
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-9">
+              <h1>test</h1>
+            </div>
+            <div className="col-md-3">
+              <div className="sidebar">
+                <p>test</p>  
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- project/src/index.js
+++ project/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { store, history} from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+
+import App from './components/App';
+
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+
+), document.getElementById('root'));
